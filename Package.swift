f5f1258b713db70e9b67f93982dccb73fff804cc
ketzusaka <james@weebly.com>
2016-07-05 23:51:28 -0700

    Update dependencies to compile on 06-20

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporMustache",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12)
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 13)
     ]
 )
