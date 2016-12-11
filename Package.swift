41c93b62b520e6985bf18a81da005f8ca4ccd039
Saito Yuta <kateinoigakukun@gmail.com>
2016-12-11 20:14:21 +0900

    Update dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporMustache",
     dependencies: [
-        .Package(url: "https://github.com/nsleader/Mustache.git", majorVersion: 0, minor: 10),
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/nsleader/Mustache.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 11)
     ]
 )
