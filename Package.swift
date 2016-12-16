00de3158ec74f093175d9a657d72bf1d35501522
Saito Yuta <kateinoigakukun@gmail.com>
2016-12-16 21:55:12 +0900

    Update Vapor

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporMustache",
     dependencies: [
         .Package(url: "https://github.com/kateinoigakukun/Mustache.git", majorVersion: 0),
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
     ]
 )
